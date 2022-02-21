8dab69d4f9309d59ac805b1caf1a25873206958c
Felipe <lipe0503@gmail.com>
2022-02-21 16:30:42 -0300

    Teste connection mongoose OK

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 const express = require("express");
+import * as database from "./database/mongoconnection";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (_req: any, res: any) => {
+app.get("/", async (_req: any, res: any) => {
+  const db = (await database.connectionDB()).connection.collection("teste");
+  console.log(await db.findOne({ name: "teste" }));
   res.json({
     application: "Hello World",
     status: 200,
