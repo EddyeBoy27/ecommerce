2d51e02a26de652b1cadb808c695eabad324d769
Felipe <lipe0503@gmail.com>
2022-02-23 05:21:05 -0300

    refactor: export de variáveis

--- src/database/mongoconnection.ts
+++ src/database/mongoconnection.ts
@@ -1,7 +1,9 @@
 import mongoose, { Mongoose } from "mongoose";
 import config from "../config";
 
-export const connectionDB = async (): Promise<Mongoose> =>
+const connectionDB = async (): Promise<Mongoose> =>
   mongoose.connect(config.mongo.url, config.mongo.options);
 
-export const closeDB = (): Promise<void> => mongoose.connection.close();
+const closeDB = (): Promise<void> => mongoose.connection.close();
+
+export { connectionDB, closeDB };
