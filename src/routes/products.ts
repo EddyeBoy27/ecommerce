d2e2c98349999afe512168d2d4215f9f0ba13c1b
Felipe <lipe0503@gmail.com>
2022-02-23 15:24:52 -0300

    refactor: edit de rotas

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { getAllProducts } from "../controllers/products.controller";
-import { connectionDB } from "../database";
 
 const products: Router = Router();
 
