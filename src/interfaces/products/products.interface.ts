170af66913300dd68149624677c23843b1655e52
Felipe <lipe0503@gmail.com>
2022-02-23 05:30:59 -0300

    feat: cria interface

---
+++ src/interfaces/products/products.interface.ts
@@ -0,0 +1,6 @@
+import { Product } from "./single.product.interface";
+
+export interface Products {
+  items: Product[];
+  totalEntries: number;
+}
---
+++ src/interfaces/products/single.product.interface.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  title: string;
+  description: string;
+  image: string;
+  value: number;
+  stars: number;
+  discountCoupon: boolean;
+  inventory: number;
+  category: string;
+}
