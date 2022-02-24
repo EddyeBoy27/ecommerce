9955e68890a3f57262f97ce67128432031307d44
Felipe <lipe0503@gmail.com>
2022-02-24 03:47:45 -0300

    feat: cria rota de createProduct

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getAllProducts } from "../controllers/products.controller";
+import { getAllProducts, createProduct } from "../controllers/products.controller";
 
 const products: Router = Router();
 
 // Products Routes
 products.get("/", getAllProducts);
+products.post("/new", createProduct);
 
 export { products };
