ab93a4f9bdaf59999220695ee5c9a832c10129d1
Felipe <lipe0503@gmail.com>
2022-02-24 00:36:20 -0300

    refactor: cleaning code product.controller

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
-import { connectionDB } from "../database";
-import { Products } from "../interfaces/products/products.interface";
+import { IProducts } from "../interfaces/products/products.interface";
 import * as productService from "../services";
 
-export const getAllProducts = async (req: Request, res: Response): Promise<Response<Products>> => {
-  // const db = (await connectionDB()).connection.collection("teste");
-  const pd = await productService.getAllProducts();
-  return res.status(200).json(pd);
+export const getAllProducts = async (
+  _req: Request,
+  res: Response
+): Promise<Response<IProducts>> => {
+  const prodServAll = await productService.getAllProducts();
+  return res.status(200).json(prodServAll);
 };
