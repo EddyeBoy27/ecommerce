2c162eb7b25ea727acc8da90fc0ddd595b528b2c
Felipe <lipe0503@gmail.com>
2022-02-24 03:46:51 -0300

    feat: cria handler de CREATE Product

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { IProducts } from "../interfaces/products/products.interface";
+import { IProduct } from "../interfaces/products/single.product.interface";
 import * as productService from "../services";
+import * as middleware from "../middlewares";
+import { productSchema } from "../middlewares/schemas";
 
 export const getAllProducts = async (
   _req: Request,
@@ -8,4 +11,10 @@ export const getAllProducts = async (
 ): Promise<Response<IProducts>> => {
   const prodServAll = await productService.getAllProducts();
   return res.status(200).json(prodServAll);
+};
+
+export const createProduct = async (req: Request, res: Response): Promise<Response<IProduct>> => {
+  await middleware.validate(req.body, productSchema);
+  const createProduct = await productService.createProduct<IProduct>(req.body);
+  return res.status(201).json(createProduct);
 };
